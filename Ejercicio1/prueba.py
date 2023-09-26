8178604576bae30ed0d64ad2fd682227b6c5955e
albabernal03 <ALBABERO@MYUAX.COM>
2023-09-26 19:40:16 +0200

    c

--- Ejercicio1/prueba.py
+++ Ejercicio1/prueba.py
@@ -1,20 +1,18 @@
 class Matriz():
-    def __init__(self,elementos: list):
+    def __init__(self, elementos: list):
         self.elementos = elementos
 
 class Transpuesta(Matriz):
-    def __init__(self,elementos: list):
-        super().__init__(elementos) #este super es para heredar el constructor de la clase padre, que seria Matriz
+    def __init__(self, elementos: list):
+        super().__init__(elementos)
         self.elementos = elementos
 
     def transpuesta(self):
-        return [[self.elementos[j][i] for j in range(len(self.elementos))] for i in range(len(self.elementos[0]))] #esto es una funcion lambda que significa lo mismo que el for 
-    #en este caso es correcto usar la funcion ya que no estamos trabajando con grandes volunes de datos
-    #si manejamos muchos datos es mejor usar el for, ya que la funcion lambda es mas lenta
+        return [[self.elementos[j][i] for j in range(len(self.elementos))] for i in range(len(self.elementos[0]))]
 
 class Imprimir(Matriz):
-    def __init__(self,elementos: list):
-        super().__init__(elementos) #este super es para heredar el constructor de la clase padre, que seria Matriz
+    def __init__(self, elementos: list):
+        super().__init__(elementos)
         self.elementos = elementos
 
     def imprimir(self):
@@ -22,13 +20,12 @@ class Imprimir(Matriz):
             print(fila)
 
 class Lanzador(Imprimir, Transpuesta):
-    #Creame funcion que permita llamar a las funciones imprimir y transpuesta de las clases Imprimir y Transpuesta y recoja los datos con un input
     def __init__(self):
-        self.elementos=[]
+        self.elementos = []
         self.cantidad_filas = int(input("Ingrese la cantidad de filas: "))
         self.cantidad_columnas = int(input("Ingrese la cantidad de columnas: "))
         self.crear_matriz()
-        super().__init__(self.elementos) 
+        super().__init__(self.elementos)
 
     def crear_matriz(self):
         for i in range(self.cantidad_filas):
@@ -41,10 +38,8 @@ class Lanzador(Imprimir, Transpuesta):
         print('La matriz es: ')
         self.imprimir()
         print("La matriz transpuesta es: ")
-        self.transpuesta().imprimir()
-
-        
-
+        transpuesta = self.transpuesta()
+        Imprimir(transpuesta).imprimir()
 
 class Main():
     def __init__(self):
