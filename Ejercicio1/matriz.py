fb637a46c88a6b8bac96b12e58d847940a2077ea
albabernal03 <ALBABERO@MYUAX.COM>
2023-09-26 18:58:27 +0200

    c

--- Ejercicio1/matriz.py
+++ Ejercicio1/matriz.py
@@ -8,6 +8,16 @@ class Transpuesta(Matriz):
         self.elementos = elementos
 
     def transpuesta(self):
-        return [[self.elementos[j][i] for j in range(len(self.elementos))] for i in range(len(self.elementos[0]))]
+        return [[self.elementos[j][i] for j in range(len(self.elementos))] for i in range(len(self.elementos[0]))] #esto es una funcion lambda que significa lo mismo que el for 
     
 
+class Imprimir(Matriz):
+    def __init__(self,elementos: list):
+        super().__init__(elementos) #este super es para heredar el constructor de la clase padre, que seria Matriz
+        self.elementos = elementos
+
+    def imprimir(self):
+        for i in range(len(self.elementos)):
+            for j in range(len(self.elementos[0])):
+                print(self.elementos[i][j], end=" ")
+            print()
