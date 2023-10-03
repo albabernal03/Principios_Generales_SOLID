f62e4e3689da4a8d48604dd108b73a0b83064c1a
albabernal03 <ALBABERO@MYUAX.COM>
2023-10-03 22:24:33 +0200

    cambios

--- Ejercicio2/Authenticator.py
+++
@@ -1,3 +0,0 @@
-class Authenticator:
-    def __init__(self, user_database):
-        self.user_database = user_database
--- Ejercicio2/PaymentProcessor.py
+++
@@ -1,4 +0,0 @@
-
-class PaymentProcessor:
-    def __init__(self, api_key):
-        self.api_key = api_key
